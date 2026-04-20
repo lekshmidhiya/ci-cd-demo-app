a552c10c3136244b72737974052f1188ab965d9d
Lekshmi <lekshmidhiya@gmail.com>
2026-04-20 22:22:29 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('CI/CD Pipeline Working Successfully');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
